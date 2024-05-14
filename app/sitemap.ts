1d766fb7dbfecca74937b08e123bb4cd6ab8b07f
Adriano Martins de Oliveira Sousa <adrianomartins002@gmail.com>
2024-05-14 08:02:31 +0200

    fix - remove the blogs files

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,17 +1,12 @@
-import { getBlogPosts } from 'app/aboutme/utils'
 
 export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/aboutme/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
-
+  
   let routes = ['', '/aboutme'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
 
-  return [...routes, ...blogs]
+  return [...routes]
 }
